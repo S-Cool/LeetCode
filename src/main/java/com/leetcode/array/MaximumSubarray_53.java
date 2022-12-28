aeb7ff695d81c2509dbdd5db9d7570242c6ec29f
skuliashou <sergey.kyjleshov@gmail.com>
2022-12-28 17:08:20 +0100

    53 - Maximum Subarray

--- src/main/java/com/leetcode/array/MaximumSubarray_53.java
+++ src/main/java/com/leetcode/array/MaximumSubarray_53.java
@@ -15,13 +15,19 @@ package com.leetcode.array;
 //        Input: nums = [5,4,-1,7,8]
 //        Output: 23
 
-public class MaximumSubarray53 {
+public class MaximumSubarray_53 {
     public static void main(String[] args) {
         int[] array = {-2,1,-3,4,-1,2,1,-5,4};
         int maxSubArray = maxSubArray(array);
         System.out.println("Result: " + maxSubArray);
     }
 
+    /**
+     * Time complexity O(n) / Space complexity O(1)
+     *
+     * @param nums
+     * @return
+     */
     public static int maxSubArray(int[] nums) {
         int max = Integer.MIN_VALUE;
         int sum = 0;
@@ -32,6 +38,29 @@ public class MaximumSubarray53 {
 
             if (sum < 0) {
                 sum = 0;
+            }
+        }
+        return max;
+    }
+
+    /**
+     * Brute Force variant
+     * Time complexity O(n^2) / Space complexity O(1)
+     *
+     * @param inputArray
+     * @return
+     */
+    public static int findMaxSum(int[] inputArray) {
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+
+        for (int i = 0; i < inputArray.length; i++) {
+            sum = 0;
+            for (int j = i; j < inputArray.length; j++) {
+                sum += inputArray[j];
+                if (sum > max) {
+                    max = sum;
+                }
             }
         }
         return max;

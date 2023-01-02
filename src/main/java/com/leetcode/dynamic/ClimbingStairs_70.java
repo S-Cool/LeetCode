3307f4375a2cec5c3668cb846467585afbf3c3fe
skuliashou <sergey.kyjleshov@gmail.com>
2023-01-02 16:56:34 +0100

    70 - Climbing Stairs

---
+++ src/main/java/com/leetcode/dynamic/ClimbingStairs_70.java
@@ -0,0 +1,49 @@
+package com.leetcode.dynamic;
+
+//        You are climbing a staircase. It takes n steps to reach the top.
+//
+//        Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+//
+//        Example 1:
+//        Input: n = 2
+//        Output: 2
+//        Explanation: There are two ways to climb to the top.
+//        1. 1 step + 1 step
+//        2. 2 steps
+//
+//        Example 2:
+//        Input: n = 3
+//        Output: 3
+//        Explanation: There are three ways to climb to the top.
+//        1. 1 step + 1 step + 1 step
+//        2. 1 step + 2 steps
+//        3. 2 steps + 1 step
+
+public class ClimbingStairs_70 {
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(5));
+    }
+
+    /**
+     * Time complexity: O(n)
+     * Space complexity: O(1)
+     *
+     * @param n
+     * @return
+     */
+    public static int climbStairs(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+
+        int one = 1;
+        int two = 1;
+        for (int i = 0; i < n - 1; i++) {
+            int temp = one;
+            one = one + two;
+            two = temp;
+        }
+        return one;
+    }
+}

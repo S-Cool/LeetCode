b4a937eedf3ecc257d4c981a8b1325160153dd97
skuliashou <sergey.kyjleshov@gmail.com>
2023-01-30 19:57:59 +0100

    73 - Set Matrix Zeroes

--- src/main/java/com/leetcode/matrix/SetMatrixZeroes_73.java
+++ src/main/java/com/leetcode/matrix/SetMatrixZeroes_73.java
@@ -16,18 +16,21 @@ import java.util.List;
 
 public class SetMatrixZeroes_73 {
     public static void main(String[] args) {
-        int matrix[][] = {{1, 0, 1},
+        int[][] matrix = {{1, 0, 1},
+                {1, 0, 1},
+                {1, 1, 1}};
+
+        int[][] matrixSecond = {{1, 0, 1},
                 {1, 0, 1},
                 {1, 1, 1}};
 
         setZeroes(matrix);
 
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
+        printMatrix(matrix);
+
+        setZeroesSpaceEfficient(matrixSecond);
+
+        printMatrix(matrixSecond);
     }
 
     /**
@@ -55,6 +58,61 @@ public class SetMatrixZeroes_73 {
                     matrix[i][j] = 0;
                 }
             }
+        }
+    }
+
+    /**
+     * Time complexity : O(m*n)
+     * Space complexity : O(1)
+     *
+     * @param matrix
+     */
+    public static void setZeroesSpaceEfficient(int[][] matrix) {
+        boolean isCol = false;
+        int rowLength = matrix.length;
+        int columnLength = matrix[0].length;
+
+        for (int i = 0; i < rowLength; i++) {
+            if (matrix[i][0] == 0) {
+                isCol = true;
+            }
+            for (int j = 1; j < columnLength; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[0][j] = 0;
+                    matrix[i][0] = 0;
+                }
+            }
+        }
+
+        for (int i = 1; i < rowLength; i++) {
+            for (int j = 1; j < columnLength; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // See if the first row needs to be set to zero as well
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < columnLength; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // See if the first column needs to be set to zero as well
+        if (isCol) {
+            for (int i = 0; i < rowLength; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
+    private static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }

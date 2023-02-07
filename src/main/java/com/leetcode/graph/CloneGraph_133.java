e98bb9122762b1778930ead64dc4eb3b6be524d9
skuliashou <sergey.kyjleshov@gmail.com>
2023-02-07 11:52:05 +0100

    133 - Clone Graph

--- src/main/java/com/leetcode/graph/CloneGraph_133.java
+++ src/main/java/com/leetcode/graph/CloneGraph_133.java
@@ -1,14 +1,10 @@
 package com.leetcode.graph;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class CloneGraph_133 {
 
     public static void main(String[] args) {
-
         Node node1 = new Node(1);
         Node node2 = new Node(2);
         Node node3 = new Node(3);
@@ -26,14 +22,29 @@ public class CloneGraph_133 {
         node4.neighbors.add(node1);
         node4.neighbors.add(node3);
 
-
         CloneGraph_133 graph = new CloneGraph_133();
-        Node cloneGraph = graph.cloneGraph(node1);
+        System.out.println("Initial Graph:");
+        printGraph(node1);
+
+        Node cloneGraphRecursive = graph.cloneGraphRecursive(node1);
+        System.out.println("\nCloned Recursive Graph:");
+        printGraph(cloneGraphRecursive);
+
+        Node cloneGraphIterative = graph.cloneGraphIterative(node1);
+        System.out.println("\nCloned Iterative Graph:");
+        printGraph(cloneGraphIterative);
     }
 
     private Map<Integer, Node> map = new HashMap<>();
 
-    public Node cloneGraph(Node node) {
+    /**
+     * Time complexity: O(n)
+     * Space complexity: O(n)
+     *
+     * @param node
+     * @return
+     */
+    public Node cloneGraphRecursive(Node node) {
         return clone(node);
     }
 
@@ -54,6 +65,66 @@ public class CloneGraph_133 {
         return clone;
     }
 
+    /**
+     * Time complexity: O(n)
+     * Space complexity: O(n)
+     *
+     * @param node
+     * @return
+     */
+    public Node cloneGraphIterative(Node node) {
+        if (node == null) {
+            return null;
+        }
+
+        Node cloneNode = new Node(node.val);
+
+        Map<Integer, Node> nodeHashMap = new HashMap<>();
+        nodeHashMap.put(cloneNode.val, cloneNode);
+
+        Deque<Node> queue = new LinkedList<>();
+        queue.add(node);
+
+        while (!queue.isEmpty()) {
+            Node n = queue.pop();
+            for (Node neighbor : n.neighbors) {
+                if (!nodeHashMap.containsKey(neighbor.val)) {
+                    nodeHashMap.put(neighbor.val, new Node(neighbor.val));
+                    queue.add(neighbor);
+                }
+                nodeHashMap.get(n.val).neighbors.add(nodeHashMap.get(neighbor.val));
+            }
+        }
+        return cloneNode;
+    }
+
+    private static void printGraph(Node node) {
+        if (node != null) {
+            Node cloneNode = new Node(node.val);
+
+            Map<Integer, Node> nodeHashMap = new HashMap<>();
+            nodeHashMap.put(cloneNode.val, cloneNode);
+
+            Deque<Node> queue = new LinkedList<>();
+            queue.add(node);
+
+            while (!queue.isEmpty()) {
+                Node n = queue.pop();
+                for (Node neighbor : n.neighbors) {
+                    if (!nodeHashMap.containsKey(neighbor.val)) {
+                        nodeHashMap.put(neighbor.val, new Node(neighbor.val));
+                        queue.add(neighbor);
+                    }
+                    nodeHashMap.get(n.val).neighbors.add(nodeHashMap.get(neighbor.val));
+                }
+            }
+
+            for (Map.Entry<Integer, Node> entry : nodeHashMap.entrySet()) {
+                System.out.println("Key: " + entry.getKey() + "[" + entry.getValue().neighbors.toString() + "]");
+            }
+        }
+    }
+
     private static class Node {
         private int val;
         private List<Node> neighbors;
@@ -71,6 +142,13 @@ public class CloneGraph_133 {
         public Node(int val, List<Node> neighbors) {
             this.val = val;
             this.neighbors = neighbors;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "val=" + val +
+                    '}';
         }
     }
 }

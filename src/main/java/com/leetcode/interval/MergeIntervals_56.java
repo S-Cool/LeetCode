9e89ca610e3c55675b46122946d13f52be764ab1
skuliashou <sergey.kyjleshov@gmail.com>
2023-02-16 17:52:02 +0100

    56 - Merge Intervals

--- src/main/java/com/leetcode/interval/MergeIntervals_56.java
+++ src/main/java/com/leetcode/interval/MergeIntervals_56.java
@@ -36,12 +36,13 @@ public class MergeIntervals_56 {
     public static int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         LinkedList<int[]> merged = new LinkedList<>();
+        merged.add(intervals[0]);
 
-        for (int[] interval : intervals) {
-            if (merged.isEmpty() || merged.getLast()[1] < interval[0]) {
-                merged.add(interval);
+        for (int i = 1; i < intervals.length; i++) {
+            if (merged.getLast()[1] < intervals[i][0]) {
+                merged.add(intervals[i]);
             } else {
-                merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
+                merged.getLast()[1] = Math.max(merged.getLast()[1], intervals[i][1]);
             }
         }
 

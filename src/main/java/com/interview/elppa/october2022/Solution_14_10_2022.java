fc7dab17c88b11cf09d5bfce9d4e29ccfed0b3d8
skuliashou <sergey.kyjleshov@gmail.com>
2024-02-22 23:16:37 +0100

    interview elppa 14.10.2022

--- src/main/java/com/interview/elppa/october2022/Solution_14_10_2022.java
+++ src/main/java/com/interview/elppa/october2022/Solution_14_10_2022.java
@@ -14,9 +14,17 @@ import java.util.Map;
 
 public class Solution_14_10_2022 {
 
+    /**
+     * Time complexity: O(W*N*M)
+     * Space complexity: O(n)
+     *
+     * @param initString - initial String
+     * @param arrayString - array of String to find
+     * @return true if at least one word from arrayString is found
+     */
     public static boolean findWordsFromString(String initString, String[] arrayString) {
         Map<Character, List<Integer>> map = new HashMap<>();
-        for (int i = 0; i < initString.toCharArray().length; i++) {
+        for (int i = 0; i < initString.length(); i++) {
             if (map.containsKey(initString.charAt(i))) {
                 List<Integer> arrayList = map.get(initString.charAt(i));
                 arrayList.add(i);
@@ -27,12 +35,14 @@ public class Solution_14_10_2022 {
                 map.put(initString.charAt(i), indexList);
             }
         }
-
+        // Number of words - W
         for (String oneOfSearchingWord : arrayString) {
             char[] chars = oneOfSearchingWord.toCharArray();
             if (map.containsKey(chars[0])) {
+                // Size of the string - N
                 List<Integer> integers = map.get(chars[0]);
                 for (Integer in : integers) {
+                    // Max length of a word - M
                     String substring = initString.substring(in, in + oneOfSearchingWord.length());
                     if (oneOfSearchingWord.equals(substring)) {
                         return true;
